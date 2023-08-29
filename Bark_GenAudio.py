b2193f500fd06ef016f676db6a2e5da6ed0513bb
Samir Chaman Serna <80238298+SamirChamanSerna@users.noreply.github.com>
2023-08-28 22:16:56 -0600

    Más pruebas.

--- Bark_GenAudio.py
+++ Bark_GenAudio.py
@@ -16,28 +16,42 @@ model = model.to(device)
 
 # load in fp16
 # No parece que funciona pero el archivo de audio no contiene nada
+# Necesita optimum
 # model = BarkModel.from_pretrained("suno/bark-small", torch_dtype=torch.float16).to(device)
 
 # convert to bettertransformer
 # Al parecer esto hace que el modelo completo alucine más.
-# model = model.to_bettertransformer()
+# can gain 20% to 30% in speed with zero performance degradation.
+model = model.to_bettertransformer()
 
 # enable CPU offload
-model.enable_cpu_offload()
+# 80% reduction in memory footprint
+# model.enable_cpu_offload()
 
 # Generating speech
 processor = AutoProcessor.from_pretrained("suno/bark")
 
-voice_preset = "v2/es_speaker_0"
+voice_preset = "v2/es_speaker_4"
 
 inputs = processor(
-    "Hola, esto es una prueba con el modelo grande.",
+    "Hola, esto es algo más avanzado.",
     voice_preset=voice_preset,
 )
+# Basic
+# audio_array = model.generate(**inputs)
 
-audio_array = model.generate(**inputs)
+# Advanced
+audio_array = model.generate(
+    **inputs,
+    # num_beams=4,
+    temperature=1,
+    semantic_temperature=0.85,
+    do_sample=True,
+    early_stopping=False
+)
+
 audio_array = audio_array.cpu().numpy().squeeze()
 
 # save them as a .wav file
 sample_rate = model.generation_config.sample_rate
-scipy.io.wavfile.write("bark_out_Test4.wav", rate=sample_rate, data=audio_array)
+scipy.io.wavfile.write("bark_out_Test16.wav", rate=sample_rate, data=audio_array)
